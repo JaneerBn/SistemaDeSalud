3bcf3aa5e136c4e413fb7803217ee3128dc4f49d
Janeer Barrera <barrera.janeer@gmail.com>
2025-04-13 18:46:27 -0500

    CRUD contenido completo

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+{{-- resources/views/dashboard.blade.php --}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Bienvenido') }}
+        </h2>
+
+        <p>Este es nuestro Proyecto Janeer Barrera y William Villa</p>
+
+    </x-slot>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+{{-- resources/views/layouts/app.blade.php --}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+</head>
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            @yield('content') <!-- Aquí se debe mostrar el contenido de cada vista -->
+        </main>
+    </div>
+</body>
+</html>
